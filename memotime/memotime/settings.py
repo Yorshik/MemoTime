815c73dd2fc5bb30d1fcbe05539a8431617698af
Максим Бердников <frixit@ya.ru>
2024-12-01 22:02:51 +0300

    hot-fix: codestyle

--- memotime/memotime/settings.py
+++ memotime/memotime/settings.py
@@ -81,9 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": decouple.config("MEMOTIME_DATABASE_NAME", default="MemoTime"),
         "USER": decouple.config("MEMOTIME_DATABASE_USER", default=None),
-        "PASSWORD": decouple.config(
-            "MEMOTIME_DATABASE_PASSWORD", default=None
-        ),
+        "PASSWORD": decouple.config("MEMOTIME_DATABASE_PASSWORD", default=None),
         "HOST": decouple.config("MEMOTIME_DATABASE_HOST", default="postgres"),
         "PORT": decouple.config("MEMOTIME_DATABASE_PORT", default="5432"),
     },
@@ -91,22 +89,18 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": (
-            "django.contrib.auth.password_validation.MinimumLengthValidator"
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
         ),
     },
     {
-        "NAME": (
-            "django.contrib.auth.password_validation.CommonPasswordValidator"
-        ),
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": (
-            "django.contrib.auth.password_validation.NumericPasswordValidator"
-        ),
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
