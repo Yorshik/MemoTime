ea1c8c435f0ae1b22535574fdec04d50ad2abb6c
Максим Бердников <frixit@ya.ru>
2024-12-01 22:06:44 +0300

    hot-fix: user name in config for pos

--- memotime/memotime/settings.py
+++ memotime/memotime/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": decouple.config("MEMOTIME_DATABASE_NAME", default="MemoTime"),
-        "USER": decouple.config("MEMOTIME_DATABASE_USER", default=None),
+        "USER": decouple.config("MEMOTIME_DATABASE_USER", default="postgres"),
         "PASSWORD": decouple.config("MEMOTIME_DATABASE_PASSWORD", default=None),
         "HOST": decouple.config("MEMOTIME_DATABASE_HOST", default="postgres"),
         "PORT": decouple.config("MEMOTIME_DATABASE_PORT", default="5432"),
