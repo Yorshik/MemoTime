3d9d6a7c29041b6b77615653de685ed333b1c051
Кирилл Чистяков <kirill125790zzz@yandex.ru>
2024-12-02 23:38:08 +0300

    hot-fix: changed name of homepage template

--- memotime/apps/homepage/views.py
+++ memotime/apps/homepage/views.py
@@ -2,7 +2,7 @@ import django.views.generic
 
 
 class HomeView(django.views.generic.TemplateView):
-    template_name = "homepage/homepage.html"
+    template_name = "homepage/index.html"
 
 
 __all__ = ()
