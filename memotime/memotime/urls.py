6622fd2d1966d1341166d7fb033ebb27409f2dbd
Максим Бердников <frixit@ya.ru>
2024-12-18 17:50:59 +0300

    hot-fix: user app

--- memotime/memotime/urls.py
+++ memotime/memotime/urls.py
@@ -1,4 +1,3 @@
-import captcha.urls
 import django.conf
 import django.conf.urls.i18n
 import django.conf.urls.static
@@ -7,7 +6,6 @@ import django.urls
 
 import apps.feedback.urls
 import apps.homepage.urls
-import apps.schedule
 import apps.schedule.urls
 import apps.users.urls
 
@@ -15,6 +13,7 @@ urlpatterns = [
     django.urls.path("", django.urls.include(apps.homepage.urls)),
     django.urls.path("feedback/", django.urls.include(apps.feedback.urls)),
     django.urls.path("schedule/", django.urls.include(apps.schedule.urls)),
+    django.urls.path("users/", django.urls.include(apps.users.urls)),
     django.urls.path(
         "i18n/",
         django.urls.include(
@@ -22,7 +21,6 @@ urlpatterns = [
         ),
     ),
     django.urls.path("admin/", django.contrib.admin.site.urls),
-    django.urls.path("captcha", django.urls.include(captcha.urls)),
 ]
 
 if django.conf.settings.DEBUG:
